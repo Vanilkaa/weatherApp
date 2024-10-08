2c61fea07e23af9610b6c48e393bc65653e2a50e
Vanilka <gamvanin@gmail.com>
2024-10-08 12:01:13 +0300

    Add basic functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './style.css'
 import key from './APIKey.js'
+
+const form = document.querySelector('form')
+const cityInput = document.querySelector('input')
+const weatherDisplay = document.querySelector('p')
+
+async function getWeather(city) {
+    let data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}`);
+    try {
+        data = await data.json();
+    } catch {
+        data = undefined;
+    }
+    return(!data ? new Weather(city, 'na') : new Weather(city, data.currentConditions.conditions));
+}
+
+function Weather(city, weather) {
+    this.city = city;
+    this.weather = weather;
+}
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    let weatherData = await getWeather(formData.get('city'));
+    weatherDisplay.textContent = weatherData.weather;
+    cityInput.value = '';
+})
